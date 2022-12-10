182dae7e840fc42228fee5fcf2a6ad269a535726
dovmes <120055177+dovmes@users.noreply.github.com>
2022-12-10 23:17:47 +0200

    Temp fix for delete.c

--- delete.c
+++ delete.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "functions.h"
 #include "sqlite\sqlite3.h"
 
 void delete() {
     sqlite3 *db;
     sqlite3_open("data/db.db", &db);
 
-    sqlite3_stmt *stmt;
-    int rc = sqlite3_prepare_v2(db, "select count(*) from product", -1, &stmt, NULL);
-    rc = sqlite3_step(stmt);
-    int count = sqlite3_column_int(stmt, 0);
-
-    int index = 0;
+    int index = -1;
     for (;;) {
         printf("Enter number of product which you want to delete: ");
         if (scanf("%d", &index) == 1 && getchar() == '\n') {
@@ -24,14 +17,13 @@ void delete() {
                 ;
         }
     }
-    char *query = malloc(1000);
-    sprintf(query, "delete from product where rowid = (select rowid from product limit 1 offset %d)", index + 1);
+    char *query = calloc(1000, 1);
+    sprintf(query, "delete from product where rowid = (select rowid from product limit 1 offset %d)", index);
     if (sqlite3_exec(db, query, NULL, NULL, NULL)) {
         printf("Error\n");
     } else {
         printf("Success\n");
     }
-
     free(query);
     sqlite3_close(db);
     return;
